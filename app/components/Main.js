cf3a28aac3460ae0a87ac769d78563d2a4f00db2
Bill Donovan <williamcdonovan@gmail.com>
2017-09-21 11:52:20 -0600

    added Main component

---
+++ app/components/Main.js
@@ -0,0 +1,18 @@
+var React = require('react');
+
+//var Search = require('./Search');
+
+const Main = React.createClass({
+
+	getInitialState: function () {
+		return{
+			//states
+		};
+	},
+	render: function () {
+
+	}
+
+});
+
+module.exports = Main;
