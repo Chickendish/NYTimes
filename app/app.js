7e32581cc5db80ee778530cb56f42bf094ba0c2e
Bill Donovan <williamcdonovan@gmail.com>
2017-09-20 21:16:12 -0600

    added server.js file

--- app/app.js
+++ app/app.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var Router = require('react-router');
 
 
 ReactDOM.render(<div>Hello World</div>, document.getElementById('app'));
