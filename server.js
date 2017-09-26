d062bdd266a39741dd801c3313c6ea081f2d2180
Bill Donovan <williamcdonovan@gmail.com>
2017-09-25 19:02:04 -0600

    added routes to server.js

--- server.js
+++ server.js
@@ -1,7 +1,33 @@
+//===========================================
+// Dependencies
+//===========================================
+
 const express = require('express');
 const bodyParser = require('body-parser');
-const logger = require('morgan');
-const mongojs = require('mongojs');
+//const logger = require('morgan');
+const mongoose = require('mongoose');
 
+//===========================================
+// Instantiate server
+//===========================================
 
+const app = express();
+PORT = process.env.PORT || 3600;
+
+//===========================================
+// Run morgan for logging
+//===========================================
+
+//app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+//===========================================
+// Listen on server
+//===========================================
+
+app.listen(PORT, ()=>
+	console.log("App listening on port: " + PORT));
 
