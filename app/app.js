93b0bef6f8dc14b890bff26b08de9f7ed393e158
Bill Donovan <williamcdonovan@gmail.com>
2017-09-25 19:31:38 -0600

    Added Main react component

--- app/app.js
+++ app/app.js
@@ -1,6 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var Router = require('react-router');
+var Main = require('./components/Main');
 
 
 ReactDOM.render(<Main />, document.getElementById('app'));
